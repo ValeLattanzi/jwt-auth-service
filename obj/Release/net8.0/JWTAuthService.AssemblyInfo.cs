31ae2a66d389774f0f786091a565a07bcdce58d1
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-08-03 16:13:57 -0300

    publish

--- obj/Release/net8.0/JWTAuthService.AssemblyInfo.cs
+++ obj/Release/net8.0/JWTAuthService.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWTAuthService")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0c063a8a459b0e3f427d91a2d563c0cbe84065")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWTAuthService")]
